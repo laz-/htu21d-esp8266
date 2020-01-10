cd5fe43e8c2749e6a32cf51d2366c11b5edb6998
adam lazur <laz@fb.com>
2020-01-11 00:05:13 +0900

    tweaks to enable basic read functionality

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # htu21d-esp8266
 This is a micropython module / class to measure data from the htu21d
+
+# Example (D1 Mini)
+
+This code works on a D1 Mini, SCL on pin labeled D1, SDA on pin labeled D2.
+
+```
+scl = machine.Pin(5, machine.Pin.OPEN_DRAIN, machine.Pin.PULL_UP)
+sda = machine.Pin(4, machine.Pin.OPEN_DRAIN, machine.Pin.PULL_UP)
+htu = htu21d.HTU21D(scl, sda)
+while True:
+    print('temp %f humid %f' % (htu.temperature, htu.humidity))
+    time.sleep(2)
+```
--- htu21d.py
+++ htu21d.py
@@ -1,26 +1,26 @@
 from machine import I2C, Pin
-
+import time
 
 class HTU21D(object):
-    ADDRESS = 0x40
-    ISSUE_TEMP_ADDRESS = 0xE3
-    ISSUE_HU_ADDRESS = 0xE5
+    I2CADDR = 0x40
+    CMD_READTEMP = 0xE3
+    CMD_READHUM = 0xE5
+    CMD_WRITEREG = 0xE6
+    CMD_READREG = 0xE7
+    CMD_RESET = 0xFE
 
-    def __init__(self, scl, sda):
-        """Initiate the HUT21D
-
-        Args:
-            scl (int): Pin id where the sdl pin is connected to
-            sda (int): Pin id where the sda pin is connected to
-        """
-        self.i2c = I2C(scl=Pin(scl), sda=Pin(sda), freq=100000)
+    def __init__(self, scl, sda, freq=10000):
+        self.i2c = I2C(scl=scl, sda=sda, freq=freq)
+        self.reset()
+        # User registers should be 0x02 after a reset
+        assert(self.userreg == b'\x02')
 
     def _crc_check(self, value):
         """CRC check data
 
         Notes:
             stolen from https://github.com/sparkfun/HTU21D_Breakout
-        
+
         Args:
             value (bytearray): data to be checked for validity
 
@@ -41,32 +41,33 @@ class HTU21D(object):
         else:
             return False
 
-    def _issue_measurement(self, write_address):
-        """Issue a measurement.
-
-        Args:
-            write_address (int): address to write to
-        :return:
-        """
-        self.i2c.start()
-        self.i2c.writeto_mem(int(self.ADDRESS), int(write_address), '')
-        self.i2c.stop()
-        data = bytearray(3)
-        self.i2c.readfrom_into(self.ADDRESS, data)
+    def _issue_measurement(self, command):
+        assert(self.i2c.writeto(self.I2CADDR, bytes([command])) == 1)
+        time.sleep_ms(50) # gotta give it time to do its thing
+        data = self.i2c.readfrom(self.I2CADDR, 3)
         if not self._crc_check(data):
             raise ValueError()
         raw = (data[0] << 8) + data[1]
         raw &= 0xFFFC
         return raw
 
+    def reset(self):
+        self.i2c.writeto(self.I2CADDR, bytes([self.CMD_RESET]))
+        return
+
     @property
     def temperature(self):
         """Calculate temperature"""
-        raw = self._issue_measurement(self.ISSUE_TEMP_ADDRESS)
+        raw = self._issue_measurement(self.CMD_READTEMP)
         return -46.85 + (175.72 * raw / 65536)
 
     @property
     def humidity(self):
         """Calculate humidity"""
-        raw =  self._issue_measurement(self.ISSUE_HU_ADDRESS)
+        raw =  self._issue_measurement(self.CMD_READHUM)
         return -6 + (125.0 * raw / 65536)
+
+    @property
+    def userreg(self):
+        """User Register"""
+        return self.i2c.readfrom_mem(self.I2CADDR, self.CMD_READREG, 1)
